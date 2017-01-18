e82753b4cbb9bf818e3b2f11d4e24a7c4acdd47c
Eason <Eason@DESKTOP-7B4OB8N>
2017-01-19 03:20:45 +0800

    參考91大大的Issues修正版本。

--- homework_Exercise/Product.cs
+++ homework_Exercise/Product.cs
@@ -14,27 +14,27 @@ namespace homework_Exercise
         /// https://msdn.microsoft.com/zh-tw/library/cc981895(v=vs.110).aspx
         /// </summary>
 
-        public static IEnumerable<int> GroupSum(this IEnumerable<ProductModel> ProductModel, int GroupCount, string column)
+        public static IEnumerable<int> GroupSum<T>(this IEnumerable<T> products, int mete, Func<T, int> column)
         {
-            int AllCount = ProductModel.Count();
-            if (AllCount == 0) 
+            int TotalCount = products.Count();
+            if (TotalCount == 0)
                 throw new InvalidOperationException("Cannot compute median for an empty set.");
 
-            var sortedList = ProductModel.Select(column);
+            var temp = products.Select(column);
 
-            int cont = 0;            
-            while (cont < AllCount)
+            int counter = 0;
+            while (counter < TotalCount)
             {
                 int value = 0;
-                var results1 = sortedList.Skip(cont).Take(GroupCount);
+                var results1 = temp.Skip(counter).Take(mete);
                 foreach (int v in results1) value += v;
-                cont += GroupCount;
+                counter += mete;
                 yield return value;
             }
         }
     }
 
-    public class ProductModel
+    public class ProductItem
     {
         public int Id { get; set; }
         public int Cost { get; set; }
--- homework_ExerciseTests/ProductTests.cs
+++ homework_ExerciseTests/ProductTests.cs
@@ -12,28 +12,28 @@ namespace homework_Exercise.Tests
     [TestClass()]
     public class ProductTests
     {
-        private List<ProductModel> Products = new List<ProductModel>();
+        private List<ProductItem> products = new List<ProductItem>();
 
         [TestInitialize()]
         public void MyTestInitialize()
         {
-            Products.Add(new ProductModel { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 });
-            Products.Add(new ProductModel { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 });
-            Products.Add(new ProductModel { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 });
-            Products.Add(new ProductModel { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 });
-            Products.Add(new ProductModel { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 });
-            Products.Add(new ProductModel { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 });
-            Products.Add(new ProductModel { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 });
-            Products.Add(new ProductModel { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 });
-            Products.Add(new ProductModel { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 });
-            Products.Add(new ProductModel { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 });
-            Products.Add(new ProductModel { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 });
+            products.Add(new ProductItem { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 });
+            products.Add(new ProductItem { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 });
+            products.Add(new ProductItem { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 });
+            products.Add(new ProductItem { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 });
+            products.Add(new ProductItem { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 });
+            products.Add(new ProductItem { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 });
+            products.Add(new ProductItem { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 });
+            products.Add(new ProductItem { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 });
+            products.Add(new ProductItem { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 });
+            products.Add(new ProductItem { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 });
+            products.Add(new ProductItem { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 });
         }
 
         [TestCleanup()]
         public void MyTestCleanup()
         {
-            Products.Clear();
+            products.Clear();
         }
 
         [TestMethod()]
@@ -42,9 +42,9 @@ namespace homework_Exercise.Tests
             //Arrange
             var expected = new List<int> { 6, 15, 24, 21 };
             //Act
-            var actual = Product.GroupSum(Products, 3, "Cost");
+            var actual = Product.GroupSum(products, 3, p => p.Cost);
             //Assert
-            expected.ShouldBeEquivalentTo(actual);
+            actual.ShouldBeEquivalentTo(expected);
         }
 
         [TestMethod()]
@@ -53,9 +53,9 @@ namespace homework_Exercise.Tests
             //Arrange
             var expected = new List<int> { 50, 66, 60 };
             //Act
-            var actual = Product.GroupSum(Products, 4, "Revenue");
+            var actual = Product.GroupSum(products, 4, p => p.Revenue);
             //Assert
-            expected.ShouldBeEquivalentTo(actual);
+            actual.ShouldBeEquivalentTo(expected);
         }
 
         [TestMethod()]
@@ -64,9 +64,9 @@ namespace homework_Exercise.Tests
             //Arrange
             var expected = new List<int> { 115, 140, 31 };
             //Act
-            var actual = Product.GroupSum(Products, 5, "SellPrice");
+            var actual = Product.GroupSum(products, 5, p => p.SellPrice);
             //Assert
-            expected.ShouldBeEquivalentTo(actual);
+            actual.ShouldBeEquivalentTo(expected);
         }
 
         [TestMethod()]
@@ -75,9 +75,9 @@ namespace homework_Exercise.Tests
             //Arrange
             var expected = new List<int> { 66 };
             //Act
-            var actual = Product.GroupSum(Products, 99, "Cost");
+            var actual = Product.GroupSum(products, 99, p => p.Cost);
             //Assert
-            expected.ShouldBeEquivalentTo(actual);
+            actual.ShouldBeEquivalentTo(expected);
         }
     }
 }
